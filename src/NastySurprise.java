00adb34eecc6cfc179223dcad23fbc09906b1ac6
League <League@AdminisorsiMac2.attlocal.net>
2018-11-17 11:27:04 -0800

    NastySurprise & SimonSays (IP)

--- src/NastySurprise.java
+++ src/NastySurprise.java
@@ -1,35 +1,53 @@
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class NastySurprise implements MouseListener{
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();	
+	JButton trick = new JButton();
+	JButton treat = new JButton();
+	JLabel label = new JLabel();
+	JLabel labels = new JLabel();
 	public static void main(String[] args) {
 		NastySurprise NS = new NastySurprise();
 	}
 	
 	public NastySurprise() {
-		JFrame frame = new JFrame();
 		frame.setVisible(true);
-		JPanel panel = new JPanel();	
 		frame.add(panel);
-		JButton trick = new JButton();
 		frame.add(panel);		
 		trick.addMouseListener(this);
 		panel.add(trick);
-		JButton treat = new JButton();
 		panel.add(treat);
 		treat.addMouseListener(this);
-		JLabel label = new JLabel();
 		label.setText("Trick");
 		trick.add(label);
-		JLabel labels = new JLabel();
 		labels.setText("Treat");
 		treat.add(labels);
 	}
 	
+	private void showPictureFromTheInternet(String imageUrl) {
+	     try {
+	          URL url = new URL(imageUrl);
+	          Icon icon = new ImageIcon(url);
+	          JLabel imageLabel = new JLabel(icon);
+	          JFrame frame = new JFrame();
+	          frame.add(imageLabel);
+	          frame.setVisible(true);
+	          frame.pack();
+	     } catch (MalformedURLException e) {
+	          e.printStackTrace();
+	     }
+	}
 	
 	@Override
 	public void mouseClicked(MouseEvent e) {
@@ -38,8 +56,12 @@ public class NastySurprise implements MouseListener{
 	}
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		if(e.getSource().equals(trick)) {
+			showPictureFromTheInternet("https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12193133/German-Shepherd-Puppy-Fetch.jpg");
+		}
+		else if(e.getSource().equals(treat)) {
+			showPictureFromTheInternet("https://1.bp.blogspot.com/-ym8M046R3yg/VugWOXBZYRI/AAAAAAAAJYQ/DcVK8wOK0PYsApO73-s6T3_b4IJrpaaLQ/s1600/4A.jpg");
+		}
 	}
 	@Override
 	public void mouseReleased(MouseEvent e) {
